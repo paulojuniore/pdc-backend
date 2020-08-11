b0a5cc37ab0397b12d931924db497687d970be67
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-08-11 19:38:43 -0300

    Added endpoint /evadidos thats return the number of dropout students per period.

--- controllers/statistics_controller.py
+++ controllers/statistics_controller.py
@@ -9,6 +9,22 @@ from util import constants
 
 connection = Connection()
 
+@routes.route("/evadidos/<int:id_motivo>")
+def escaped_from_periodo(id_motivo):
+  query = 'SELECT semestre_vinculo, count(*) AS qtd_evadidos\
+    FROM "DiscenteVinculo"\
+    WHERE id_curso=' + str(constants.COMPUTACAO_KEY) + \
+    ' AND id_situacao_vinculo=' + str(id_motivo) + '\
+    GROUP BY semestre_vinculo\
+    ORDER BY semestre_vinculo'
+  result = connection.select(query)
+
+  response = []
+  for i in range(len(result)):
+    response.append({ "semestre_vinculo": result[i][0], "qtd_evadidos": result[i][1] })
+
+  return jsonify(response)
+
 @routes.route("/egressos")
 def graduates_by_period():
   query = 'SELECT semestre_vinculo, count(*) AS qtd_egressos\
