c99aae9e9ad2d640f1096ac4d626250ef0a86c3f
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-08-12 20:21:42 -0300

    obtaining a reduced part of the periods provided on the route

--- controllers/statistics_controller.py
+++ controllers/statistics_controller.py
@@ -2,12 +2,11 @@
 import sys
 sys.path.append("./connection")
 from Connection import Connection
-from flask import jsonify
+from flask import jsonify, request
 from . import routes
 from util import constants
 
 connection = Connection()
-
 
 # Rota responsável por retornar o número de alunos evadidos do curso de Computação agrupados
 #por período, onde o id_motivo deve ser um valor entre 1 e 9, inclusive.
@@ -30,7 +29,6 @@ def escaped_from_periodo(id_motivo):
   else:
     return { "error": "Invalid resource" }
 
-
 # Rota responsável por retornar o número de alunos egressos (formados) do curso de 
 #Computação agrupados por período.
 @routes.route("/egressos")
@@ -48,3 +46,27 @@ def graduates_by_period():
     response.append({ "semestre_vinculo": result[i][0], "qtd_egressos": result[i][1] })
 
   return jsonify(response)
+
+@routes.route("/egressos/<minimo>/<maximo>")
+def graduates_by_interval_of_periods(maximo, minimo):
+  query = 'SELECT semestre_vinculo, count(*) AS qtd_egressos\
+    FROM "DiscenteVinculo"\
+    WHERE id_curso=' + str(constants.COMPUTACAO_KEY) + \
+    ' AND id_situacao_vinculo=' + str(constants.GRADUADO) + '\
+    GROUP BY semestre_vinculo\
+    ORDER BY semestre_vinculo'
+  result = connection.select(query)
+  
+  min_index = 0
+  max_index = 0
+  for i in range(len(result)):
+    if (result[i][0] == minimo):
+      min_index = i
+    elif (result[i][0] == maximo):
+      max_index = i
+
+  result_splitted = result[min_index:max_index+1]
+
+  print (result_splitted)
+
+  return { "min_index": min_index, "max_index": max_index }
