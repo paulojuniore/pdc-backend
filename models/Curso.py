0214428c48df794d893eec2f9e3c54ec4e329d80
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-09-11 16:16:08 -0300

    Added amount of active periods to the json response.

--- models/Curso.py
+++ models/Curso.py
@@ -6,11 +6,35 @@ from flask import jsonify
 from util import constants
 
 class Curso():
-  connection = None
 
   def __init__(self):
     # Instância da conexão ao banco de dados.
     self.connection = Connection()
+
+  # Função que conta o número de períodos que o aluno está ativo no curso.
+  def count_number_of_periods(self, ano_ingresso, semestre_ingresso):
+    #Contador de  periodos ativos
+    cpa = 0
+
+    while ano_ingresso < int(constants.ANO_ATUAL):
+      while semestre_ingresso <= 2:
+        semestre_ingresso += 1
+        cpa += 1
+      semestre_ingresso = 1 
+      ano_ingresso += 1
+
+    # coletando a diferença quando AI == AA
+    cpa += int(constants.SEMESTRE_ATUAL) - 1
+    result = [0, False]
+
+    # Caso seja fera
+    if cpa == 0 or cpa > 14:
+        result = [cpa, False]
+    # Caso não seja fera
+    elif (cpa >= 1):
+        result = [cpa, True]
+
+    return result
 
   # Função que retorna informações sobre os alunos ativos do curso de Computação,
   ## informações estas que são a matrícula do aluno e a porcentagem concluída do 
@@ -34,9 +58,23 @@ class Curso():
 
     json_return = []
     for registro in result:
-      percent = (registro[1] * 100) / int(constants.TOTAL_CREDITOS)
-      periodo_ingresso = str(registro[0][1:3]) + "." + str(registro[0][3])
-      json_return.append({ "matricula": registro[0], "periodo_ingresso": periodo_ingresso,
-        "porcentagem_concluida": round(percent, 2) })
+      # percentual do curso concluído, com base na quantidade de créditos cursados. 
+      percentual_concluido = (registro[1] * 100) / int(constants.TOTAL_CREDITOS)
+
+      ano_ingresso = registro[0][1:3]
+      semestre_ingresso = registro[0][3]
+
+      periodo_ingresso = ano_ingresso + "." + semestre_ingresso
+
+      # retorna uma lista com dois elementos, onde o primeiro é o número de períodos
+      ## que o aluno está ativo e o segundo é um boolean que indica se ele é fera (False)
+      ### ou não (True)
+      periodos_ativos = self.count_number_of_periods(int(ano_ingresso), int(semestre_ingresso))
+      
+      # o aluno ativo só é adicionado a resposta json se ele não é um fera, ou seja, não
+      ## ingressou no período atual.
+      if (periodos_ativos[1]):
+        json_return.append({ "matricula": registro[0], "periodo_ingresso": periodo_ingresso,
+          "porcentagem_concluida": round(percentual_concluido, 2), "periodos_ativos": periodos_ativos[0] })
 
     return jsonify(json_return)
